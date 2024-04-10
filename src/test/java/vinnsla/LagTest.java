cdf4bbf831eefa9131b8d2f7f61328c2c969fcb2
Dagur01 <dagurgudna@gmail.com>
2024-04-10 09:13:09 +0000

    h

---
+++ src/test/java/vinnsla/LagTest.java
@@ -0,0 +1,37 @@
+package vinnsla;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LagTest {
+
+    private String nafn;
+
+    @Test
+    public void testToString(){
+        this.nafn="test";
+        assertTrue(nafn.equals("test"));
+    }
+
+    @Test
+    public void testGetMedia() {
+
+    }
+
+    @Test
+    public void testGetNafn(){
+
+    }
+
+    @Test
+    public void testGetMynd(){
+
+    }
+
+    @Test
+    public void testGetLengd(){
+
+    }
+
+}
---
+++ src/test/java/vinnsla/LagalistiTest.java
@@ -0,0 +1,26 @@
+package vinnsla;
+
+import org.junit.jupiter.api.Test;
+
+public class LagalistiTest {
+
+    @Test
+    public void testNaesti(){
+
+    }
+
+    @Test
+    public void testGetListi(){
+
+    }
+
+    @Test
+    public void testSetIndex(){
+
+    }
+
+    @Test
+    public void testGetIndex(){
+
+    }
+}
