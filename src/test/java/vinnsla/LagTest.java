cf9decd380bdf7fc9097a3f0e7af1b792a0067ae
Dagur01 <dagurgudna@gmail.com>
2024-04-10 21:13:21 +0000

    lagtest

--- src/test/java/vinnsla/LagTest.java
+++ src/test/java/vinnsla/LagTest.java
@@ -2,11 +2,15 @@ package vinnsla;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LagTest {
 
-    private String nafn;
+    private String nafn = "test";
+    private String media = "media";
+    private int lengd = 1;
+    private String mynd = "mynd";
 
     @Test
     public void testToString(){
@@ -16,21 +20,23 @@ public class LagTest {
 
     @Test
     public void testGetMedia() {
-
+        assertTrue(media.equals("media"));
     }
 
     @Test
     public void testGetNafn(){
-
+        assertTrue(nafn.equals("test"));
     }
 
     @Test
     public void testGetMynd(){
-
+        assertTrue(mynd.equals("mynd"));
     }
 
     @Test
     public void testGetLengd(){
+        assertTrue(lengd==1);
+        assertFalse(lengd!=1);
 
     }
 
--- src/test/java/vinnsla/LagalistiTest.java
+++ src/test/java/vinnsla/LagalistiTest.java
@@ -1,8 +1,15 @@
 package vinnsla;
 
+import is.vinnsla.Lag;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
 
 public class LagalistiTest {
+
+    protected ObservableList<Lag> listi = FXCollections.observableArrayList();
+
+    private int index = 0;
 
     @Test
     public void testNaesti(){
